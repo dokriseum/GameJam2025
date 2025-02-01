87441392de53c2db8c763ca63636abf206019e26
Tim <timmeytube@gmail.com>
2025-02-01 22:51:25 +0100

    working skill tree

--- Assets/Scripts/PublicOpinion.cs
+++ Assets/Scripts/PublicOpinion.cs
@@ -1,5 +1,7 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PublicOpinion : MonoBehaviour
 {
@@ -8,9 +10,13 @@ public class PublicOpinion : MonoBehaviour
     private int currentOpinion;
     private int opinionToSpend;
 
+    public TextMeshProUGUI opinionText;
+    public Image opinionBar;
+
     public void Start()
    {
       instance = this;
+      currentOpinion = opinionToSpend = 100;
    }
 
     public void IncreaseOpinion(int byScore)
