b8a09a124406170d70f0f078c412a471badcce04
buecherwurm772 <186387784Buecherwurm772@noreply.github.com>
2025-02-01 16:49:26 +0100

    Zwischenstand 1

--- Assets/NewEmptyCSharpScript.cs
+++ Assets/NewEmptyCSharpScript.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewEmptyCSharpScript
+public class PlayerController : MonoBehaviour
 {
-    
+    private Vector2 moveDirection;
+
+    void Update()
+    {
+        // Bewegungsrichtung über die Pfeiltasten der Tastatur erfassen
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        // Die Bewegungsrichtung verwenden, um den Spieler zu bewegen
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Den Spieler in die entsprechende Richtung bewegen
+        float moveSpeed = 5f;
+        Vector3 movement = new Vector3(moveDirection.x, 0f, moveDirection.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
 }
