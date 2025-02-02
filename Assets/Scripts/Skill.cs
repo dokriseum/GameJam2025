16e0c8006f7f126f7c9b8ed7b5181d0c999c1959
dokriseum <eiki300499@googlemail.com>
2025-02-02 10:15:24 +0100

    Fähigkeiten werden freigeschaltet und Antworten können daran ausgewählt werden

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -14,17 +14,17 @@ public class Skill : MonoBehaviour
 
     private void Start()
     {
-        if(linkedSkill == null)
+        if (linkedSkill == null)
         {
             Debug.LogWarning(gameObject.name + ": linkedSkill is not set!");
             return;
         }
-        if(iconImg == null)
+        if (iconImg == null)
         {
             Debug.LogWarning(gameObject.name + ": iconImg is not set!");
             return;
         }
-        if(text == null)
+        if (text == null)
         {
             Debug.LogWarning(gameObject.name + ": text is not set!");
             return;
