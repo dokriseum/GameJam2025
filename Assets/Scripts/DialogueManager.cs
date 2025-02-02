f2cb95881bb83b418145563f3831b596a3370e48
Tim <timmeytube@gmail.com>
2025-02-02 10:28:31 +0100

    working dialogue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using UnityEditor.ShortcutManagement;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public Transform spawnArea;
     public GameObject dialoguePrefabModerator, dialoguePrefabPolitician, empty;
     public GameObject replyOptions;
 
+    public ScrollRect scrollRect;
+
 
     private GameObject currentReplyGO;
     public GameObject[] replyIcons;
@@ -22,10 +25,12 @@ public Transform spawnArea;
 
     public void BeginGame()
     {
+        currentIndex = 0;
+        replyOptions.SetActive(false);
         StartCoroutine(StartNewDialogue());
     }
 
-    public void InstantiateNewDialogue(Skill_SO modifier)
+    public void StartReply(Skill_SO modifier)
     {
         replyOptions.SetActive(false);
 
@@ -38,7 +43,18 @@ public Transform spawnArea;
             SetTextForReply(dialogParameters[currentIndex].neutraleAntwort);
         }
 
+        StartCoroutine(WaitAfterReplyChosen());
+    }
+
+    private void GoToNextQuestion()
+    {
         currentIndex++;
+        StartCoroutine(StartNewDialogue());
+    }
+
+    public void StartReply()
+    {
+        StartReply(null);
     }
 
     public void SetTextForReply(string reply)
@@ -48,23 +64,46 @@ public Transform spawnArea;
 
     private IEnumerator StartNewDialogue()
     {
-        Instantiate(dialoguePrefabModerator, spawnArea);
+        Debug.Log("Starting new dia");
+
+        replyOptions.SetActive(false);
+        GameObject modQuestion = Instantiate(dialoguePrefabModerator, spawnArea);
+        SetQuestionByModerator(modQuestion.GetComponentInChildren<TextMeshProUGUI>(), dialogParameters[currentIndex].Moderationsfrage);
         Instantiate(empty, spawnArea);
+        ScrollToBottom();
         yield return new WaitForSeconds(waitingTimeAfterModeratorAsked);
         Instantiate(empty, spawnArea);
         currentReplyGO = Instantiate(dialoguePrefabPolitician, spawnArea);
+        ScrollToBottom();
         replyOptions.SetActive(true);
         UpdateReplyOptions();
     }
 
+    public void ScrollToBottom()
+    {
+        Canvas.ForceUpdateCanvases();
+
+        scrollRect.verticalNormalizedPosition = 0 ;
+    }
+
     public void UpdateReplyOptions()
     {
+        TextMeshProUGUI neutralText = replyOptions.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        neutralText.text = dialogParameters[currentIndex].neutraleAntwort;
+
         int possibleModsThisReply = dialogParameters[currentIndex].possibleModifications.Length;
-        for(int i = 0; i < 2; i++)
+        /*
+        for(int i = 0; i < 1; i++)
         {
             replyIcons[i].SetActive(true);
             replyIcons[i].GetComponent<SkillReply>().InstantiateSkill(dialogParameters[currentIndex].possibleModifications[i]);
         }
+        */
+    }
+
+    public void SetQuestionByModerator(TextMeshProUGUI toTextObj, string withReply)
+    {
+        toTextObj.text = withReply;
     }
 
     private IEnumerator KIAntwort(Skill_SO modifier, int index)
@@ -77,6 +116,14 @@ public Transform spawnArea;
          yield return new WaitForSeconds(2f);
           
          // Juchu, antwort ist fertig gebacken
-         SetTextForReply(hierKommtDieAntwortRein);
+         SetTextForReply(hierKommtDieAntwortRein);         
+    }
+
+    private IEnumerator WaitAfterReplyChosen()
+    {
+        yield return new WaitForSeconds(waitingTimeAfterModeratorAsked);
+        Debug.Log("Done waiting");
+        GoToNextQuestion();
+
     }
 }
